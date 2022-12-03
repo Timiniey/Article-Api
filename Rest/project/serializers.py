4392a23d52b517411324ceeea2598171f9619265
Timiniey <119698451+Timiniey@users.noreply.github.com>
2022-12-03 07:33:14 +0000

    Create serializers.py

---
+++ Rest/project/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers 
+from . models import Article
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Article
+        fields="__all__"
